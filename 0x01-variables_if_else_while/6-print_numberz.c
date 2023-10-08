07ec9f7f6919b0181e9f061145a4299000ef8c3c
ilhamyusufkibiya <rabiayusufkibiya@gmail.com>
2023-10-07 21:42:19 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print a number from 0 to 9.
+ *
+ * Return - Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchr('\n');
+	return (0);
+}
+
