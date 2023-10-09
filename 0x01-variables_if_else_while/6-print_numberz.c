ee90e29ffb842147f1269c1693d8a73ff264ed98
ilhamyusufkibiya <rabiayusufkibiya@gmail.com>
2023-10-09 16:32:16 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 /**
  * main - print a number from 0 to 9.
  *
- * Return - Always 0
+ * Return: Always 0
  */
 int main(void)
 {
@@ -11,7 +11,7 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(i);
+		putchar(i + '0');
 	}
 	putchar('\n');
 	return (0);
