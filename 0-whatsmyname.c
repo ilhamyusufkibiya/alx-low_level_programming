1f284ff959d2e5da73b54eaa9662241ee7f1e623
ilhamyusufkibiya <rabiayusufkibiya@gmail.com>
2023-10-31 17:47:45 -0700

    0-whatsmyname.c

---
+++ 0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Aprogram that prints its name
+ * @argc: Argument passed
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
--- 0x07-pointers_arrays_strings/README.md
+++
@@ -1,1 +0,0 @@
-this folder cointains more about pointers and arrays 0x07-pointers_arrays_strings
