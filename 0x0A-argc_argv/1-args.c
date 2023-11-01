b0d3a7377e06802f867cd33f719ba79ae9518823
ilhamyusufkibiya <rabiayusufkibiya@gmail.com>
2023-11-01 00:25:08 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints number of argument
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: o
+ */
+int main(int argc, char *argv[])
+
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
