f83ae155e2526fe42e90132afab9f6b63b827641
ilhamyusufkibiya <rabiayusufkibiya@gmail.com>
2023-10-31 17:58:37 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the file name
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: o
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
