a3007ee970452638e93c2e50f4481e009937834c
ilhamyusufkibiya <rabiayusufkibiya@gmail.com>
2023-11-20 15:36:33 -0800

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "list.h"
+/**
+ * list_len - prints the number of elements in a link
+ * @h: head node
+ * Return: length of node
+ */
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		elements++;
+	}
+	return (elements);
+}
+
